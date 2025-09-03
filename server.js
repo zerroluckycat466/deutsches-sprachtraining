b5d5a86ce2255c5231415b8a968407a315056cdd
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-04 01:23:09 +0800

    Added logging for HTTP requests

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Middleware für die Protokollierung von HTTP-Anfragen
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // MongoDB Verbindung
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/deutsches-sprachtraining';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
